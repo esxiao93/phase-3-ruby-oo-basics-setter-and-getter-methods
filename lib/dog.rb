d147b61b5a7014f8bd7833f4717748e804e1a3c4
Eric Xiao <es.xiao93@gmail.com>
2022-05-23 14:55:42 -0400

    commit tree 7aab3cb7144a77346fbdaece2dff343b20548bc2

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
+    def name=(name)
+        @name = name
+    end
 
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
 end
+
+thor = Dog.new
+thor.name = ("Thor")
+thor.breed = ("GoldenRetriever")
+
+thor.name
+thor.breed
